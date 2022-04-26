02aefbe924006546a736b0a4f0b51dda3a6265cb
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-04-27 00:27:46 +0300

    created signup page and events on htags

--- Android-app/ParkWire/app/src/main/java/parkwire/com/LoginActivity.java
+++ Android-app/ParkWire/app/src/main/java/parkwire/com/LoginActivity.java
@@ -1,7 +1,10 @@
 package parkwire.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Add "already have an account btn listener
+        TextView btn = findViewById(R.id.create_acc_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
 }
--- Android-app/ParkWire/app/src/main/java/parkwire/com/RegisterActivity.java
+++ Android-app/ParkWire/app/src/main/java/parkwire/com/RegisterActivity.java
@@ -1,7 +1,10 @@
 package parkwire.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Add "already have an account btn listener
+        TextView btn = findViewById(R.id.have_account_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
