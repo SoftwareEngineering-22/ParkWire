9d9725e2bdf34e4851b59bec2f168f5b42207d1d
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-04-27 16:58:54 +0300

    Created home page

--- Android-app/ParkWire/app/src/main/java/parkwire/com/LoginActivity.java
+++ Android-app/ParkWire/app/src/main/java/parkwire/com/LoginActivity.java
@@ -1,5 +1,4 @@
 package parkwire.com;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +12,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // log in forward
+         TextView loginBtn = findViewById(R.id.login_btn);
+         loginBtn.setOnClickListener(new View.OnClickListener(){
+             public void onClick(View v) {
+                 startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+             }
+         });
+
         // Add "already have an account btn listener
         TextView btn = findViewById(R.id.create_acc_btn);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -20,6 +27,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
-
     }
 }
