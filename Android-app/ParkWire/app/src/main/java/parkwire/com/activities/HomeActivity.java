5065ed78e410b3fc6155bacc71627d7fc9e31ab6
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-05-29 21:50:32 +0300

    Implement Database Class

--- Android-app/ParkWire/app/src/main/java/parkwire/com/activities/HomeActivity.java
+++ Android-app/ParkWire/app/src/main/java/parkwire/com/activities/HomeActivity.java
@@ -14,15 +14,9 @@ import android.widget.TextView;
 import java.sql.*;
 
 import parkwire.com.R;
+import parkwire.com.models.Database;
 
 public class HomeActivity extends AppCompatActivity {
-    private Connection connection = null;
-    private final String host = "ec2-34-247-172-149.eu-west-1.compute.amazonaws.com";
-    private final String database = "d20nd037ds0s85";
-    private final int port = 5432;
-    private final String user = "qtuiehpetqjxoe";
-    private final String pass = "fb47d2dabf861efc95b5a1d760e0a0026d0d88dc5ae79bd87188da1cf8992840";
-    private String url = "jdbc:postgresql://%s:%d/%s?sslmode=require&rejectUnauthorized=false";
     // variables
     private TextView settingsBtn;
     private TextView parkingNearMeBtn;
@@ -31,31 +25,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        this.url = String.format(this.url, this.host, this.port, this.database);
-        // example for db
 
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
-
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(url, user, pass);
-            String SQL = "SELECT * FROM users";
-            Statement s = connection.createStatement();
-            ResultSet rs = s.executeQuery(SQL);
-            int i = 1;
-            while(rs.next()){
-                while(i < 7) {
-                    System.out.print(rs.getString(i) + "    ");
-                    i++;
-                }
-                System.out.println();
-                i = 1;
-            }
-        } catch (SQLException | ClassNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        }
+
+        Database mydb = new Database();
+        mydb.printQuery("SELECT * FROM USERS;");
 
         // settings activity forward
         settingsBtn = findViewById(R.id.settings_btn);
---
+++ Android-app/ParkWire/app/src/main/java/parkwire/com/models/Database.java
@@ -0,0 +1,56 @@
+package parkwire.com.models;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Database{
+
+    private Connection connection = null;
+    private final String host = "ec2-34-247-172-149.eu-west-1.compute.amazonaws.com";
+    private final String database = "d20nd037ds0s85";
+    private final int port = 5432;
+    private final String username = "qtuiehpetqjxoe";
+    private final String password= "fb47d2dabf861efc95b5a1d760e0a0026d0d88dc5ae79bd87188da1cf8992840";
+    private final String url = "jdbc:postgresql://%s:%d/%s?sslmode=require&rejectUnauthorized=false";
+
+
+    // constructor to create total diff db con
+    public Database(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection =
+                    DriverManager.getConnection(String.format(this.url, this.host, this.port, this.database), this.username, this.password);
+            System.out.println("Succesfull Connection..");
+        }
+        catch(Exception e) {
+            System.out.println("Unsuccesful connection with db.");
+            System.out.println("Error: "+ e);
+            e.printStackTrace();
+        }
+    }
+    //method which establishes connection with  DB
+    public Connection connect() {
+        return this.connection;
+    }
+
+    public void printQuery(String q){
+        try {
+            Statement s = this.connection.createStatement();
+            ResultSet rs = s.executeQuery(q);
+            while(rs.next()){
+                while (rs.next()) {
+                    for(int i = 1; i < 4; i++)
+                        System.out.print(rs.getString(i) + " ");
+                    System.out.println();
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}//end of class dbCon
